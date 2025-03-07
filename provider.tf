8ca46de55ea671a2b2b2bc4216a5657402c3468c
MachaSreenath <sreenathshree9@gmail.com>
2025-03-07 18:17:57 +0530

    minikube terraform changes

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.89.0" #AWS Provider version, not terraform version 
+      version = "5.31.0" #AWS Provider version, not terraform version 
     }
   }
 
