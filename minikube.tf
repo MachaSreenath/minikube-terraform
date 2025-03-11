9e3ec41d177b97de0240b6fff5428b1b28fec754
MachaSreenath <sreenathshree9@gmail.com>
2025-03-11 18:53:13 +0530

    minikube terraform changes

--- minikube.tf
+++ minikube.tf
@@ -1,22 +1,22 @@
-# module "minikube" {
-#   source = "github.com/scholzj/terraform-aws-minikube"
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
 
-#   aws_region    = "us-east-1"
-#   cluster_name  = "roboshop"
-#   aws_instance_type = "t3.medium"
-#   ssh_public_key = "~/sri.pub"
-#   aws_subnet_id = "subnet-0ccf64b4b868e8797"
-#   hosted_zone = "forpractice.uno"
-#   hosted_zone_private = false
+  aws_region    = "us-east-1"
+  cluster_name  = "roboshop"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "~/sri.pub"
+  aws_subnet_id = "subnet-0ccf64b4b868e8797"
+  hosted_zone = "forpractice.uno"
+  hosted_zone_private = false
 
-#   tags = {
-#     Application = "Minikube"
-#   }
+  tags = {
+    Application = "Minikube"
+  }
 
-#   addons = [
-#     "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
-#     "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
-#     "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
-#     "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
-#   ]
-# }
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
